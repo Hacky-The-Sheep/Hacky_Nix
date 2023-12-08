404a18830c139bae3c4794633c1cd567fb020f1d
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-08 15:37:08 -0600

    fixing error

--- flake.nix
+++ flake.nix
@@ -6,38 +6,17 @@
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    spicetify-nix = {
-      url = "github:the-argus/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland.url = "github:hyprwm/hyprland";
   };
 
-  outputs = { self, nixpkgs, home-manager, spicetify-nix, ...}: 
+  outputs = { nixpkgs, home-manager, ...}: 
     let 
 
       # ----- System Settings ----- #
-      system = "x86_64-linux";
-      hostname = "hackyos";
-      timezone = "America/Chicago";
-      locale = "en_US.UTF-8";
-
-      # ----- User Settings ----- #
-      username = "hacky";
-      name = "Jon";
-      email = "jon.nguyen7@protonmail.com";
-      dotfilesDir = "~/.dotfiles";
-      wm = "hyprland";
-      term = "kitty";
-      font = "Iosevka";
-      fontPkg = pkgs.iosevka;
-      
+      system = "x86_64-linux";      
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
 
     in {
-
     nixosConfigurations = {
       hackyos = lib.nixosSystem {
         inherit system;
