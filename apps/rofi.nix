05f59f3bbd2657352ce742fc2f84f0e83b8fd372
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-08 16:06:30 -0600

    added more colors

--- apps/rofi.nix
+++ apps/rofi.nix
@@ -4,7 +4,9 @@ let
   inherit (config.lib.formats.rasi) mkLiteral;
   catppuccin_mocha = {
     "*" = {
-      background-color = mkLiteral "#00ff00";
+      background-color = mkLiteral "#1e1e2e";
+      border-color = mkLiteral "#1e1e2e";
+      selected-color = mkLiteral "#1e1e2e";
     };
   };
 in {
