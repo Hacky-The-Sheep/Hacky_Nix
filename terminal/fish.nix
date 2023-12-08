09fbedfc1e7024e05206949534871bd5a38e208b
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-07 21:17:11 -0600

    cleaned up aliases

--- terminal/fish.nix
+++ terminal/fish.nix
@@ -2,37 +2,29 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      vi = "nvim";
       ei = "exit";
-      tt = "tmux new";
-      rtv = "rtv --enable-media";
-      bb = "cd -";
-      rfo = "rm -rf";
-      vrc = "vi ~/.config/nvim/init.vim";
       python = "python3";
       nb = "newsboat";
       cat = "bat";
+
+      # Git
       ga = "git add";
       gs = "git status";
       gc = "git commit -m";
+
+      # YoutubeDL
       yty = "yt-dlp -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' ";
       ytu = "yt-dlp -o '~/MyVideos/%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s'";
       ydl = "yt-dlp --prefer-ffmpeg --merge-output-format mkv ";
       ymp4 = "yt-dlp --prefer-ffmpeg --merge-output-format mp4 ";
       ymp3 = "yt-dlp --extract-audio --audio-format mp3 ";
-      rmark = "ssh root@10.11.99.1";
-      gotti = "mpv https://www.twitch.tv/tugotti78";
-      mami = "mpv https://www.twitch.tv/donscaremami";
-      screens = "rm -f Pictures/Screenshots/*";
       ls = "exa -a --color --icons --long";
-      code = "cd ~/ExtraDrive/Coding";
-      cpy = "cd ~/ExtraDrive/Coding/Python/";
-      crst = "cd ~/ExtraDrive/Coding/rust/";
-      cpr = "cd ~/ExtraDrive/Projects/";
-      kb = "cd ~/Documents/PKM/";
       zj = "zellij";
+
+      # NixOS ❄
       hmu = "home-manager switch --flake .";
       nup = "sudo nixos-rebuild switch --flake .";
+      up = "nixos-rebuild switch --upgrade";
     };
   };
 }
