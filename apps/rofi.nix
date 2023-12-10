c0c38b48e0d75672b0bbcd97d558fdb274b59d39
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-09 22:53:51 -0600

    trying to use rasi for theming

--- apps/rofi.nix
+++ apps/rofi.nix
@@ -1,17 +1,6 @@
-{ config, ...}:
-
-let
-  inherit (config.lib.formats.rasi) mkLiteral;
-  catppuccin_mocha = {
-    "*" = {
-      background-color = mkLiteral "#1e1e2e";
-      border-color = mkLiteral "#1e1e2e";
-      selected-color = mkLiteral "#1e1e2e";
-    };
-  };
-in {
+{
   programs.rofi = {
     enable = true;
-    theme = catppuccin_mocha;
+    theme = "./themes/catppuccin_mocha.rasi";
   };
 }
