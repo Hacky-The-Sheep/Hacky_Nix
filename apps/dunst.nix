876e35c6812b4c4f62c60f1d0362fe61a5e8080d
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-15 09:02:34 -0600

    changed size of notifications

--- apps/dunst.nix
+++ apps/dunst.nix
@@ -5,8 +5,8 @@
       global = {
         origin = "top-center";
         transparency = 10;
-        width = 600;
-        height = 600;
+        width = 500;
+        height = 1000;
         background = "#1e1e2e";
         foreground = "#cdd6f4";
         timeout = 10;
