6283932ae73bc74cd6d2de8b69cfbc156c0045d3
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-16 15:17:52 -0600

    Added more options and cleaned up settings

--- apps/dunst.nix
+++ apps/dunst.nix
@@ -1,15 +1,36 @@
+{ config, ... }:
+
 {
   services.dunst = {
     enable = true;
     settings = {
       global = {
         origin = "top-center";
-        transparency = 10;
-        width = 500;
-        height = 1000;
-        background = "#1e1e2e";
-        foreground = "#cdd6f4";
+        follow = "mouse";
+        monitor = 0;
+        width = 300;
+        height = 600;
+        background = "#${config.colorScheme.colors.base01}";
+        foreground = "#${config.colorScheme.colors.base05}";
         timeout = 10;
+        font = "Iosevka 14";
+        markup = "full";
+        format = "<b>%s</b>\n%b";
+        alignment = "center";
+        vertical_alignment = "center";
+        show_indicators = "yes";
+        
+        # Show icons
+        enable_recursive_icon_lookup = true;
+        icon_position = "left";
+        min_icon_size = 32;
+
+        corner_radius = 20;
+
+        # Mouse Settings
+        mouse_left_click = "context";
+        mouse_middle_click = "do_action, close_current";
+        mouse_right_click = "close_all";
       };
     };   
   };
