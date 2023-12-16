81fbd7e341e8518e4eb01d9b5c8cf3aa446eaa91
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-16 15:18:52 -0600

    fixed colorscheme

--- terminal/btop.nix
+++ terminal/btop.nix
@@ -2,7 +2,10 @@
   programs.btop = {
     enable = true;
     settings = {
+      color_theme = "catppuccin_mocha";
       theme_background = false;
+      truecolor = true;
+      rounded_corners = true;
     };
   };
 }
