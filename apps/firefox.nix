2eb4544913f45d2a2d48cb9d4e9889ffbbad29b1
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2023-12-27 08:41:08 -0600

    fixed firefox bookmarks

--- apps/firefox.nix
+++ apps/firefox.nix
@@ -8,35 +8,41 @@
 
         bookmarks = [
           {
-            name = "Oryx";
-            tags = [ "oryx" ];
-            keyword = "oryx";
-            url = "https://configure.zsa.io/";
-          }
-          {
-            name = "MP3";
-            keyword = "mp3";
-            url = "https://free-mp3-download.net/";
-          }
-          {
-            name = "Monkeytype";
-            keyword = "mt";
-            url = "https://monkeytype.com/";
-          }
-          {
-            name = "DIM";
-            keyword = "dm";
-            url = "https://app.destinyitemmanager.com/login";
-          }
-          {
-            name = "MyNixOS";
-            keyword = "nix";
-            url = "https://mynixos.com/";
-          }
-          {
-            name = "KeyMash";
-            keyword = "km";
-            url = "https://keymash.io/";
+            name = "Bookmarks";
+            toolbar = true;
+            bookmarks = [   
+              {
+                name = "KeyMash";
+                keyword = "km";
+                url = "https://keymash.io/";
+              }
+              {
+                name = "Monkeytype";
+                keyword = "mt";
+                url = "https://monkeytype.com/";
+              }
+              {
+                name = "Oryx";
+                tags = [ "oryx" ];
+                keyword = "oryx";
+                url = "https://configure.zsa.io/";
+              }
+              {
+                name = "MP3";
+                keyword = "mp3";
+                url = "https://free-mp3-download.net/";
+              }
+              {
+                name = "DIM";
+                keyword = "dm";
+                url = "https://app.destinyitemmanager.com/login";
+              }
+              {
+                name = "MyNixOS";
+                keyword = "nix";
+                url = "https://mynixos.com/";
+              }
+            ];
           }
         ];
 
@@ -50,6 +56,7 @@
         ];
 
       settings = {
+
         "dom.security.https_only_mode" = true;
         "identity.fxaccounts.enabled" = true;
         "signon.rememberSignons" = false;
