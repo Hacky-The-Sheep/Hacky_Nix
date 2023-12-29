b22aad18db6bca0409aed2f18ccfd4cd7c4ac010
Hacky-The-Sheep <jon@jon-nguyen.dev>
2023-12-29 10:29:26 -0600

    initial upload

---
+++ users/home/users.nix
@@ -0,0 +1,20 @@
+{
+  users.users.monkey = {
+    isNormalUser = true;
+    home = "/home/monkey";
+    description = "My Monkey 🐵";
+  };
+
+  
+  users.users.foxy = {
+    isNormalUser = true;
+    home = "/home/foxy";
+    description = "My Foxy 🦊";
+  };
+
+  users.users.bri = {
+    isNormalUser = true;
+    home = "/home/bri";
+    description = "My Bri 🦋";
+  };
+}
