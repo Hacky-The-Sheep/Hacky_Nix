ef40f28b80f55445c9a86b79afab6eb3fd9f51ad
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2024-02-11 14:35:47 -0600

    simplified theming

--- terminal/kitty.nix
+++ terminal/kitty.nix
@@ -1,4 +1,4 @@
-{ config, lib, ...}:
+{ lib, ...}:
 
 {
   programs.kitty = {
@@ -6,30 +6,9 @@
     font.name = "Iosevka";  
     font.size = 15;
     shellIntegration.enableFishIntegration = true;
+    theme = "Catppuccin-Mocha";
     settings = {
       background_opacity = lib.mkForce "0.90";
-
-      # Theming
-      background = "#${config.colorScheme.colors.base01}";
-      foreground = "#${config.colorScheme.colors.base05}";
-      selection_foreground = "#${config.colorScheme.colors.base01}";
-      selection_background = "#${config.colorScheme.colors.base06}";
-
-      cursor = "#${config.colorScheme.colors.base06}";
-      cursor_text_color = "#${config.colorScheme.colors.base00}";
-
-      url_color = "#${config.colorScheme.colors.base06}";
-
-      active_border_color = "#${config.colorScheme.colors.base07}";
-      inactive_border_color = "#${config.colorScheme.colors.base01}";
-      bell_border_color = "#${config.colorScheme.colors.base0A}";
-
-      mark1_foreground = "#${config.colorScheme.colors.base01}";
-      mark1_background = "#${config.colorScheme.colors.base07}";
-      mark2_foreground = "#${config.colorScheme.colors.base01}";
-      mark2_background = "#${config.colorScheme.colors.base0E}";
-      mark3_foreground = "#${config.colorScheme.colors.base01}";
-      mark3_background = "#${config.colorScheme.colors.base0F}";
     };
   };
 }
