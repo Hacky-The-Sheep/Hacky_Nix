032d0e78bf5e8c66c7eb7e549d038b652ad66e27
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2024-05-12 22:26:31 -0500

    catppuccin

--- hosts/home/home.nix
+++ hosts/home/home.nix
@@ -23,6 +23,9 @@
   home.username = "hacky";
   home.homeDirectory = "/home/hacky";
 
+  # Must be enabled for cat/nix to work
+  xdg.enable = true;
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
     
