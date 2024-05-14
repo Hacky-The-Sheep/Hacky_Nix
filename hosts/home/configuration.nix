7b2237783b70563371ac3b6fa878954a5c520c90
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2024-05-14 04:56:34 -0500

    hyprland

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -22,9 +22,14 @@
   # Needed to run swaylock
   security.pam.services.swaylock = {};
 
-  # Gnome Keyring for Hyprland
+  # Hyprland
   security.pam.services.login.enableGnomeKeyring = true;
-  
+  programs.hyprland.enable = true;
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+    
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
