c54faebf2d123e241392aa67535c395b76ff6a01
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2024-07-17 23:48:47 -0500

    remove sound.enable

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -53,7 +53,6 @@
   programs.virt-manager.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
