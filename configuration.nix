d688fed0005bb63034672b69ecc24097ad743046
hacky <jon.nguyen7@protonmail.com>
2024-08-24 21:28:42 -0500

    clean ssh

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,6 @@
       system/gnome.nix
       system/language_servers.nix
       system/printers.nix
-      # system/ssh.nix
       system/udev.nix
     ];
 
@@ -190,27 +189,4 @@
     flavor = "mocha";
     accent = "peach";
   };
-   # programs.ssh = {
-   #  # enable = true;
-
-   #  extraConfig = ''
-
-   #   Host serv_room
-   #    HostName 192.168.2.80
-   #    Port 22
-
-   #   Host Tech_Deck
-   #    HostName 192.168.2.2
-   #    Port 22
-
-   #   Host Counseling
-   #    HostName 192.168.2.30
-   #    Port 22
-    
-   #   Host home
-   #    Hostname 10.0.0.17
-   #    Port 22
-   
-   #   '';
-   #  };
 }
