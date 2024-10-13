f4bc7877cb93cb389844ddd83c6fd47a0f7107c5
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2024-10-12 19:14:13 -0500

    fonts

--- system/fonts.nix
+++ system/fonts.nix
@@ -3,13 +3,29 @@
 {
     # Fonts
     fonts.packages = with pkgs; [
-  	fira-code
-  	fira-code-symbols
-  	fira-code-nerdfont
-   #  intel-one-mono
-  	iosevka
-    # monaspace
-    powerline-fonts
-    # source-sans-pro
-  ];
+    	fira-code
+    	fira-code-symbols
+    	fira-code-nerdfont
+     #  intel-one-mono
+    	iosevka
+      # monaspace
+      powerline-fonts
+      # source-sans-pro
+    ];
+
+    fonts = {
+      enableDefaultFonts = true;
+      fontDir.enable = true;
+      fontconfig = {
+        antialias = true;
+        cache32Bit = true;
+        defaultFonts = {
+          monospace = [ "iosevka.ttf" ];
+          sansSerif = [ "iosevka.ttf" ];
+          serif = [ "iosevka.ttf" ];
+        };
+        hinting.autohint = true;
+        hinting.enable = true;
+      };
+    };
 }
