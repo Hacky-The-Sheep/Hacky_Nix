c32f3ca4860a8784a630cb3f267c1b36d6e67e99
Hacky-The-Sheep <jon.nguyen7@protonmail.com>
2024-10-15 10:49:21 -0500

    aliases

--- terminal/nushell.nix
+++ terminal/nushell.nix
@@ -14,6 +14,13 @@
       gs = "git status";
       gc = "git commit -m";
 
+      #YouTubeDLP
+      yty = "yt-dlp -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' ";
+      ytu = "yt-dlp -o '~/MyVideos/%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s'";
+      ydl = "yt-dlp --prefer-ffmpeg --merge-output-format mkv ";
+      ymp4 = "yt-dlp --prefer-ffmpeg --merge-output-format mp4 ";
+      ymp3 = "yt-dlp --extract-audio --audio-format mp3 ";
+
       # Nix Stuff
       gecon = "nix-store --gc";
     };
